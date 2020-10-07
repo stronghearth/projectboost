446ebab18dafc40dc95cad0651bbcb2080e8efcb
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-10-07 18:36:38 -0400

    added rcsThrust and mainThrust factors for increased roataion and thrust

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // declare Rigid Body instance
+    //switches for the designer to manipulate
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 20f;
+
+    // declare Rigid Body instance and audio source instance
     Rigidbody rigidBody;
     AudioSource audioSource;
 
@@ -25,14 +29,17 @@ public class Rocket : MonoBehaviour
     private void Rotate()
     {
         rigidBody.freezeRotation = true;
-            
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false;
@@ -42,7 +49,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
