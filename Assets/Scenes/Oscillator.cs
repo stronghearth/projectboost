606dd58b867a521bf6b2789844863c846f179dcf
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-10-14 15:31:06 -0400

    added manual moving pattern to obstacle in level one; only works in unity inspector during gameplay

---
+++ Assets/Scenes/Oscillator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent] //prevents accidentally adding more than one copy of script on a GameObject
+public class Oscillator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Vector3 movementVector;
+
+    // todo remove from inspector
+    [Range(0, 1)]
+    [SerializeField]
+    float moventFactor; // 0 for not moved, 1 for fully moves
+
+    Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * moventFactor;
+        transform.position = startingPosition + offset;
+    }
+}
